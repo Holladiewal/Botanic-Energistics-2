0cd844936f231cd3e39a4edf588e49af5cf7c42d
magnus97 <magnus1997.MHO@gmail.com>
2015-08-22 17:27:56 +0200

    Lets try this one more time

---
+++ src/main/java/pct/botanic/energistics/BotanicEnergistics.java
@@ -0,0 +1,27 @@
+package pct.botanic.energistics;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import pct.botanic.energistics.references.CoreRefs;
+
+/**
+ * Created by magnus97 on 22/08/2015.
+ */
+@Mod(modid = CoreRefs.MODID, name = CoreRefs.NAME,version = CoreRefs.VERSIONS,dependencies = CoreRefs.DEPENDENCIES,acceptedMinecraftVersions = "1.7.10")
+public class BotanicEnergistics {
+
+    public static void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    public static void init(FMLInitializationEvent event){
+
+    }
+
+    public static void postInit(FMLPostInitializationEvent event){
+
+    }
+
+}
